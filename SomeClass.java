9ba9201307c701738a3b29f3d0032cd455b69778
anoop <anoopramisetti90@gmail.com>
2018-02-08 11:16:34 -0800

    added fnone

---
+++ SomeClass.java
@@ -0,0 +1,16 @@
+package gitMerging;
+
+public class SomeClass{
+	public void fnOne(){
+		System.out.println("i couldn't repair brakes");
+	}
+
+	public static void main(string[] args) {
+		SomeClass sc = new SomeClass();
+		sc.fnOne{
+
+
+	        }
+
+
+}
